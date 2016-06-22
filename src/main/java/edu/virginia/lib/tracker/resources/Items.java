5bb9b8749777778f4ad68a898c16398d28a05271
Mike Durbin <md5wz@virginia.edu>
2016-06-22 11:09:02 -0400

    First demo version.

--- src/main/java/edu/virginia/lib/tracker/resources/Items.java
+++ src/main/java/edu/virginia/lib/tracker/resources/Items.java
@@ -2,8 +2,10 @@ package edu.virginia.lib.tracker.resources;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -29,12 +31,14 @@ public class Items {
     private static final String DC_IDENTIFIER = "http://purl.org/dc/elements/1.1/identifier";
     private static final String DC_TITLE= "http://purl.org/dc/elements/1.1/title";
     private static final String VIRGO_VIEW = "http://fedora.lib.virginia.edu/preservation#hasVirgoView";
+    
+    private static final String REPOSITORY_ROOT = "http://fedora01.lib.virginia.edu:8080/fcrepo/rest/";
 
     protected FusekiReader triplestore = new FusekiReader("http://fedora01.lib.virginia.edu:8080/fuseki/fcrepo-before-crash");
     
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("/{id: [^/]*}")
+    @Path("/{id: .*}")
     public JsonArray findItem(@PathParam("id") final String id) throws IOException {
         final JsonArrayBuilder a = Json.createArrayBuilder();
         for (String uri : lookupKnownId(id)) {
@@ -56,8 +60,8 @@ public class Items {
     }
     
     private List<String> lookupKnownId(final String id) throws IOException {
-        if (triplestore.getFirstAndOnlyQueryResponse("ASK { <http://fedora01.lib.virginia.edu:8080/fcrepo/rest/" + id + "> ?p ?o }").get("_askResult").equals("true")) {
-            return Collections.singletonList("http://fedora01.lib.virginia.edu:8080/fcrepo/rest/" + id);
+        if (triplestore.getFirstAndOnlyQueryResponse("ASK { <" + REPOSITORY_ROOT + id + "> ?p ?o }").get("_askResult").equals("true")) {
+            return Collections.singletonList(REPOSITORY_ROOT + id);
         } else {
             return Collections.EMPTY_LIST;
         }
@@ -104,9 +108,18 @@ public class Items {
                 o.add("system of record", result.get("odesc") + " (" + result.get("oid") + ")");
             }
         }
-        o.add("title", titles.build());
-        o.add("id", ids.build());
-        o.add("published URLs", views.build());
+        JsonArray t = titles.build();
+        if (t.size() > 0) {
+            o.add("title", t);
+        }
+        JsonArray i = ids.build();
+        if (i.size() > 0) {
+            o.add("id", i);
+        }
+        JsonArray p = views.build();
+        if (p.size() > 0) {
+            o.add("published URLs", p);
+        }
         o.add("APTrust-bound", presPackage);
         
         // include premis events
@@ -139,11 +152,58 @@ public class Items {
             }
             events.add(event.build());
         }
-        o.add("events", events.build());
+        JsonArray e = events.build();
+        if (e.size() > 0) { 
+            o.add("events", e);
+        }
         
-        // TODO: include relevant graph... (test this with a WSLS script)
+        // include the nearby graph in a format for visjs
+        final String graphQuery = "PREFIX premis: <http://www.loc.gov/premis/rdf/v1#>\n" + 
+                "PREFIX pres: <http://fedora.lib.virginia.edu/preservation#>\n" + 
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" + 
+                "PREFIX dc: <http://purl.org/dc/elements/1.1/>\n" + 
+                "\n" + 
+                "SELECT ?p ?o ?id ?title\n" + 
+                "WHERE {\n" + 
+                "    <" + uri + "> ?p ?o .\n" + 
+                "    ?o rdf:type <http://fedora.info/definitions/v4/repository#Resource> .\n" + 
+                "  OPTIONAL { ?o dc:title ?title }\n" + 
+                "  OPTIONAL { ?o dc:identifier ?id }\n" +
+                //"  MINUS { <" + uri + ">    \n" + 
+                //"      <http://www.w3.org/ns/ldp#contains> ?o }" +
+                "}";
+        JsonArrayBuilder nodes = Json.createArrayBuilder();
+        final Set<String> nodeIds = new HashSet<String>();
+        JsonArrayBuilder edges = Json.createArrayBuilder();
+        nodes.add(Json.createObjectBuilder().add("id", id(uri)).add("label", (t.size() > 0 ? t.get(0).toString() : (i.size() > 0 ? i.get(0).toString() : uri))).build());
+        for (Map<String, String> result : triplestore.getQueryResponse(graphQuery)) {
+            final String nUri = result.get("o");
+            String label = result.get("title");
+            if (label == null || label.length() == 0) {
+                label = result.get("id");
+            }
+            if (label == null || label.length() == 0) {
+                label = id(uri);
+            }
+            if (!nodeIds.contains(nUri)) {
+                nodeIds.add(nUri);
+                nodes.add(Json.createObjectBuilder().add("color",  "gray").add("id", id(nUri)).add("label", label).build());
+            }
+            edges.add(Json.createObjectBuilder().add("to", id(nUri)).add("from", id(uri)).add("label", prefix(result.get("p"))).build());
+        }
+        o.add("_graph_nodes", nodes.build());
+        o.add("_graph_edges", edges.build());
+        
         
         return o.build();
+    }
+    
+    private String id(final String uri) {
+        return uri.replace(REPOSITORY_ROOT, "");
+    }
+    
+    private String prefix(final String uri) {
+        return uri.replace("http://www.loc.gov/premis/rdf/v1#", "premis:").replace("http://www.w3.org/ns/ldp#", "ldp:").replace("http://fedora.info/definitions/v4/repository#", "fcrepo:").replace("http://purl.org/dc/terms/", "dc:terms").replace("http://fedora.lib.virginia.edu/preservation#", "pres:").replace("http://fedora.lib.virginia.edu/wsls/relationships#", "wsls:");        
     }
     
     
